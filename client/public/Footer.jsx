5ce99298829b1e2ee29c571fae8b5922555b58b4
Aarthi RS <84577668+aarthirs@users.noreply.github.com>
2024-08-25 09:51:39 +0530

    Create Footer.jsx

---
+++ client/public/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Let's create and Explore <b>More!!</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
